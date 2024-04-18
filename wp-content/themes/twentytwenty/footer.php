719fe77c5cc95344f908198ce11a38e4851d58d5
suborgerry <gerryborodin@gmail.com>
2024-04-18 12:29:58 +0300

    footer

--- wp-content/themes/twentytwenty/footer.php
+++ wp-content/themes/twentytwenty/footer.php
@@ -13,57 +13,157 @@
  */
 
 ?>
-<footer id="site-footer" class="header-footer-group">
+</main>
+<footer class="footer">
+	<div class="footer__wrapper space-sections">
+		<div class="container">
+			<div class="footer__inner">
+				<div class="footer__block">
+					<a href="/" class="footer__logo footer__logo_mobile">
+						<img src="images/footer-logo.svg" alt="logo">
+					</a>
+					<div class="footer__content">
+						<div class="footer__wrap">
+							<a href="/" class="footer__logo">
+								<img src="images/footer-logo.svg" alt="logo">
+							</a>
+							<div class="footer__info">
+								<p class="footer__info_title fw-600">Салони</p>
+								<ul class="footer__info_list">
+									<li class="footer__info_item">
+										<p class="footer__info_adress">ТЦ 4room, Петропавлівська Борщагівка, вул. Петропавлівська, 6</p>
+										<p class="footer__info_time">Пн-Нд. 10.00-20.00</p>
+									</li>
+									<li class="footer__info_item">
+										<p class="footer__info_adress">ТЦ Аракс, Київ, вул. Кільцева дорога, 110</p>
+										<p class="footer__info_time">Пн-Нд. 10.00-20.00</p>
+									</li>
+									<li class="footer__info_item">
+										<p class="footer__info_adress">ТЦ Три Слони, Львів, вул. Яворівська, 22</p>
+										<p class="footer__info_time">Пн-Нд. 11.00-19.00</p>
+									</li>
+								</ul>
+							</div>
+							<div class="footer__info">
+								<p class="footer__info_title fw-600">Майстерня</p>
+								<ul class="footer__info_list">
+									<li class="footer__info_item">
+										<p class="footer__info_adress">Київ, вул. Генерала Шаповала, 2, офіс 555</p>
+										<p class="footer__info_time">Пн-Пт. 10.00-19.00</p>
+									</li>
+								</ul>
+							</div>
+						</div>
+						<div class="footer__menu">
+							<p class="footer__menu_title fw-600">Каталог</p>
+							<nav class="footer__menu_nav">
+								<ul class="footer__menu_list">
+									<li class="footer__menu_item">
+										<a href="#" class="footer__menu_link">Картини</a>
+									</li>
+									<li class="footer__menu_item">
+										<a href="#" class="footer__menu_link">Постери</a>
+									</li>
+									<li class="footer__menu_item">
+										<a href="#" class="footer__menu_link">Рами</a>
+									</li>
+									<li class="footer__menu_item">
+										<a href="#" class="footer__menu_link">Індивідуальне замовлення</a>
+									</li>
+									<li class="footer__menu_item">
+										<a href="#" class="footer__menu_link">Подарунковий сертифікат</a>
+									</li>
+									<li class="footer__menu_item">
+										<a href="#" class="footer__menu_link">Розпис</a>
+									</li>
+								</ul>
+							</nav>
+						</div>
+						<div class="footer__menu">
+							<p class="footer__menu_title fw-600">Інформація</p>
+							<nav class="footer__menu_nav">
+								<ul class="footer__menu_list">
+									<li class="footer__menu_item">
+										<a href="#" class="footer__menu_link">Загальна інформація</a>
+									</li>
+									<li class="footer__menu_item">
+										<a href="#" class="footer__menu_link">Послуги</a>
+									</li>
+									<li class="footer__menu_item">
+										<a href="#" class="footer__menu_link">Наша команда</a>
+									</li>
+									<li class="footer__menu_item">
+										<a href="#" class="footer__menu_link">Відео</a>
+									</li>
+									<li class="footer__menu_item">
+										<a href="#" class="footer__menu_link">Партнери</a>
+									</li>
+								</ul>
+							</nav>
+						</div>
+						<div class="footer__menu">
+							<p class="footer__menu_title fw-600">Блог</p>
+							<nav class="footer__menu_nav">
+								<ul class="footer__menu_list">
+									<li class="footer__menu_item">
+										<a href="#" class="footer__menu_link">Контакти</a>
+									</li>
+								</ul>
+							</nav>
+						</div>
+					</div>
+					<div class="footer__icons">
+						<ul class="footer__social">
+							<li class="footer__social_item">
+								<a href="#" class="footer__social_link">
+									<img src="images/icons/footer-instagram.svg" alt="instagram">
+								</a>
+							</li>
+							<li class="footer__social_item">
+								<a href="#" class="footer__social_link">
+									<img src="images/icons/footer-facebook.svg" alt="facebook">
+								</a>
+							</li>
+							<li class="footer__social_item">
+								<a href="#" class="footer__social_link">
+									<img src="images/icons/footer-youtube.svg" alt="youtube">
+								</a>
+							</li>
+							<li class="footer__social_item">
+								<a href="#" class="footer__social_link">
+									<img src="images/icons/footer-pinterest.svg" alt="pinterest">
+								</a>
+							</li>
+							<li class="footer__social_item">
+								<a href="#" class="footer__social_link">
+									<img src="images/icons/footer-etsy.svg" alt="etsy">
+								</a>
+							</li>
+						</ul>
+						<ul class="footer__payments">
+							<li class="footer__payments_item">
+								<img src="images/icons/footer-paypal.svg" alt="paypal">
+							</li>
+							<li class="footer__payments_item">
+								<img src="images/icons/footer-mastercard.svg" alt="mastercard">
+							</li>
+							<li class="footer__payments_item">
+								<img src="images/icons/footer-visa.svg" alt="visa">
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div class="footer__copyright">
+					<p>© 2023. All rights reserved.</p>
+					<a href="#">Політка конфіденційності</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
 
-	<div class="section-inner">
-
-		<div class="footer-credits">
-
-			<p class="footer-copyright">&copy;
-				<?php
-				/* translators: Copyright date format, see https://www.php.net/manual/datetime.format.php */
-				$date_format = _x('Y', 'copyright date format', 'twentytwenty');
-				if (function_exists('wp_date')) {
-					echo wp_date($date_format);
-				} else {
-					echo date_i18n($date_format);
-				}
-				?>
-				<a href="<?php echo esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
-			</p><!-- .footer-copyright -->
-
-			<?php
-			if (function_exists('the_privacy_policy_link')) {
-				the_privacy_policy_link('<p class="privacy-policy">', '</p>');
-			}
-			?>
-
-			<p class="powered-by-wordpress">
-				<a href="<?php echo esc_url(__('https://wordpress.org/', 'twentytwenty')); ?>">
-					<?php _e('Powered by WordPress', 'twentytwenty'); ?>
-				</a>
-			</p><!-- .powered-by-wordpress -->
-
-		</div><!-- .footer-credits -->
-
-		<a class="to-the-top" href="#site-header">
-			<span class="to-the-top-long">
-				<?php
-				/* translators: %s: HTML character for up arrow. */
-				printf(__('To the top %s', 'twentytwenty'), '<span class="arrow" aria-hidden="true">&uarr;</span>');
-				?>
-			</span><!-- .to-the-top-long -->
-			<span class="to-the-top-short">
-				<?php
-				/* translators: %s: HTML character for up arrow. */
-				printf(__('Up %s', 'twentytwenty'), '<span class="arrow" aria-hidden="true">&uarr;</span>');
-				?>
-			</span><!-- .to-the-top-short -->
-		</a><!-- .to-the-top -->
-
-	</div><!-- .section-inner -->
-
-</footer><!-- #site-footer -->
+<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+<script src="./wp-content/themes/twentytwenty/assets/js/global.js" defer></script>
 
 <?php wp_footer(); ?>
 
